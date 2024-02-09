72cdeff231adacaa97dd2c40b82141a5ad2ff5b0
Williams Avendaño <avendano.balarezo.williams@gmail.com>
2024-02-09 12:26:20 -0500

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();                    // Load environment variables
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB!');
+}).catch(err => {
+    console.error('Error connecting to MongoDB', err);
+}); // Connect to MongoDB
+
+const app = express();             // Create express app
 
 app.listen(3000, () => {            // Start server
   console.log('Server started on port 3000!');

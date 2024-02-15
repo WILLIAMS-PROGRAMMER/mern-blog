c7fd39082e1dbf489144c62652bdab3426ffc667
Williams Avendaño <avendano.balarezo.williams@gmail.com>
2024-02-15 11:08:15 -0500

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-creen flex items-center justify-center my-16'>
+        <div className='max-w-2xl mx-auto p-3 text-center'>
+          <div>
+            <h1 className='text-3xl font-semibold text-center my-7'>About Williams Blog</h1>
+            <div className='text-md text-gray-500 flex flex-col gap-6'>
+               <p>Williams Blog is a blog where you can read about the latest news, technology, and more.
+                You can also create an account and write your own blog posts. Williams Blog is a place where
+                you can share your thoughts and ideas with the world.</p>
+
+                <p>On this blog, you'll find weekly articles and tutorials on topics such as web development,
+                  software engineering, and programming languages. Sahand is always learning and exploring
+                  new technologies, so be sure to check back often for new content!</p>
+
+                <p>We encourage you to leave comments on our posts and engage with other readers.
+                You can like other people's comments and reply to them as well. We believe that a 
+                community of learners can help each other grow and improve.</p>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
